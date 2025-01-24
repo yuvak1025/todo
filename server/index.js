a695510d6fec36a3fb3e76505c2caed9c7454130
yuvak1025 <yuvakiran12321@gmail.com>
2025-01-24 21:18:30 +0530

    change in index.js

--- server/index.js
+++ server/index.js
@@ -8,14 +8,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "build/index.html"));
-})
+
 
 
 const router = require("./routes");
 app.use("/api", router);
+
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build/index.html"));
+})
 
 const port = 5000;
 
